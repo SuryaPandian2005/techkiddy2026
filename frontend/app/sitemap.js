4eea2fa77206b5e6d96c9fac1e6529bde754d9f8
SuryaPandian2005 <suryapandian2580@gmail.com>
2026-05-12 11:07:06 +0530

    Add sitemap for SEO

---
+++ frontend/app/sitemap.js
@@ -0,0 +1,34 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://techkidyy-sigma.vercel.app",
+      lastModified: new Date(),
+      priority: 1,
+    },
+
+    {
+      url: "https://techkidyy-sigma.vercel.app/about",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://techkidyy-sigma.vercel.app/contact",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://techkidyy-sigma.vercel.app/templates",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://techkidyy-sigma.vercel.app/login",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://techkidyy-sigma.vercel.app/register",
+      lastModified: new Date(),
+    },
+  ];
+}
